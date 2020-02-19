aaf5b8b14910367652d2cf7948f1523224864950
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2020-02-19 10:51:41 -0300

    Starting database Sequelize config

---
+++ src/database/index.js
@@ -0,0 +1,24 @@
+import Sequelize from 'sequelize';
+
+// import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  /**
+   * Vai realizar conexão com o banco de dados e exportar os módulos da aplicação
+   */
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
