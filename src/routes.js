be08f358e5e311c9c14fbf7dd5bffdc7dffceddc
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2020-02-22 17:31:55 -0300

    Challenge 1 complete

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -14,5 +15,9 @@ routes.post('/sessions', SessionController.store);
 // Middleware de autenticação. A partir daqui, todas as rotas precisam de
 // autenticação.
 routes.use(authMiddleware);
+
+routes.post('/recipients', RecipientController.store);
+routes.get('/recipients', RecipientController.index);
+routes.put('/recipients/:id', RecipientController.update);
 
 module.exports = routes;
