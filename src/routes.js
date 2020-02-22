ea2b0830579de04cc3d0e0f9c87b855dab7afe97
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2020-02-22 10:32:04 -0300

    update: new session route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const { Router } = require('express');
 
 const routes = new Router();
 
-/**
- * ROTAS DA APLICAÇÃO AQUI!!!!
- */
-// routes.get('/', {});
+routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
+
+// Middleware de autenticação. A partir daqui, todas as rotas precisam de
+// autenticação.
+routes.use(authMiddleware);
 
 module.exports = routes;
