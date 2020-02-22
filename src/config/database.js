4ca5380689006c9303847f4c016bbb9a8635cc22
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2020-02-22 10:31:21 -0300

    feat: database config

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'fastfeet',
-  database: 'postgresFastFeet',
+  database: 'fastfeet',
   define: {
     timestamps: true,
     underscored: true,
