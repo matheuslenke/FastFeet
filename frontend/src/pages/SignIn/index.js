86de975d41b2cccf777b495d1606d12f074f096b
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2020-03-30 15:54:13 -0300

    feat: login page layout done

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import logo from '~/assets/logo.png';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="FastFeet" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <p>SEU E-MAIL</p>
+        <Input name="email" type="email" placeholder="exemplo@exemplo.com" />
+        <p>SUA SENHA</p>
+        <Input name="password" type="password" placeholder="*********" />
+
+        <button type="submit">Entrar no sistema</button>
+      </Form>
+    </>
+  );
 }
