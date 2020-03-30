0675846d0588b1ce095db63571931094ff5f04ee
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2020-03-30 12:32:45 -0300

    feat: routes working

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+
+import SignIn from '../pages/SignIn';
+import OrdersList from '../pages/Orders/List';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/orders" component={OrdersList} isPrivate />
+
+      <Route path="/" component={() => <h1>404</h1>} />
+    </Switch>
+  );
+}
