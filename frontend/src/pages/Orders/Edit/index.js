e2a688e216bbbd659107034c7b7f86b447e45a39
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2020-04-07 18:25:58 -0300

    feat: orders edit

--- frontend/src/pages/Orders/Edit/index.js
+++ frontend/src/pages/Orders/Edit/index.js
@@ -1,7 +1,54 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Content } from './styles';
+import {
+  FormSection,
+  FormRow,
+  InputDiv,
+  InputLabel,
+  Input,
+  Select,
+  Option,
+} from '~/components/Form/styles';
 
-export default function Edit() {
-  return <div />;
+import BackButton from '~/components/Buttons/BackButton';
+import SaveButton from '~/components/Buttons/SaveButton';
+
+export default function OrdersForm() {
+  return (
+    <Container>
+      <Content>
+        <header>
+          <h1>Edição de Encomendas</h1>
+          <div>
+            <BackButton />
+            <SaveButton />
+          </div>
+        </header>
+
+        <FormSection>
+          <FormRow>
+            <InputDiv>
+              <InputLabel>Destinatário</InputLabel>
+              <Select type="text">
+                <Option>Opção</Option>
+              </Select>
+            </InputDiv>
+            <InputDiv>
+              <InputLabel>Entregador</InputLabel>
+              <Select type="text">
+                <Option>Opção</Option>
+              </Select>
+            </InputDiv>
+          </FormRow>
+          <FormRow>
+            <InputDiv>
+              <InputLabel>Nome do produto</InputLabel>
+              <Input type="text" placeholder="Produto" />
+            </InputDiv>
+          </FormRow>
+        </FormSection>
+      </Content>
+    </Container>
+  );
 }
