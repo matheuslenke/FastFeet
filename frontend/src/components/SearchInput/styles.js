ff16975349555f33d33e6c86ab45f8331e974cba
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2020-04-07 18:25:24 -0300

    feat: search input component

---
+++ frontend/src/components/SearchInput/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  background: #fff;
+  height: 30px;
+  width: 250px;
+  padding: 0 16px;
+  border-radius: 4px;
+  border: 1px;
+  border-color: #ddd;
+
+  svg {
+    margin-right: 8px;
+  }
+  input {
+    width: 100%;
+    border: 0;
+    background: 0;
+  }
+`;
